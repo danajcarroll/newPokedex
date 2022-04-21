86dfa71b96dbe96d50323be6bcee19d520ad19a6
Dana <danacarroll2000@gmail.com>
2022-04-21 17:02:03 -0400

    Added new css and script file to public

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
---
+++ public/script.js
@@ -0,0 +1,3 @@
+'use strict';
+
+console.log('Hey! Looks like this is working!');
