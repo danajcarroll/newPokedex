39641382d743821fadd9b8adab9183673965ef05
Dana <danacarroll2000@gmail.com>
2022-05-03 14:11:29 -0400

    Can now fetch 3 pokemon with random numbers

--- public/script.js
+++ public/script.js
@@ -1,3 +1,29 @@
 'use strict';
 
 console.log('Hey! Looks like this is working!');
+
+
+// Generating 3 featured Pokemon
+let featuredPokemonIDs = [];
+
+function getFeaturedIDs() {
+    for (let i = 1; i <= 3; i++) {
+        const randomNumber =  Math.floor(Math.random() * 898) + 1;
+        featuredPokemonIDs.push(randomNumber);
+    }
+}
+
+async function getFeaturedPokemon(arr) {
+    arr.forEach(async id => {
+        console.log(id);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await response.json();
+        console.log(data);
+    });
+}
+
+getFeaturedIDs(); // THIS FUNCTION SHOULD ONLY RUN ONCE A DAY, don't want new pokemon with every refresh
+getFeaturedPokemon(featuredPokemonIDs);
+
+
+
