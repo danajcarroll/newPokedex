b359590459fda3215d2115a0a8cda881a87d884f
Dana <danacarroll2000@gmail.com>
2022-05-03 16:10:29 -0400

    Got 3 pokemon, trying to put them on screen

--- public/script.js
+++ public/script.js
@@ -5,6 +5,8 @@ console.log('Hey! Looks like this is working!');
 
 // Generating 3 featured Pokemon
 let featuredPokemonIDs = [];
+const featBoxNodes = document.querySelectorAll('.featPokemon');
+console.log(featBoxNodes);
 
 function getFeaturedIDs() {
     for (let i = 1; i <= 3; i++) {
@@ -17,9 +19,39 @@ async function getFeaturedPokemon(arr) {
     arr.forEach(async id => {
         console.log(id);
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const data = await response.json();
-        console.log(data);
+        const data = await response.json()
+        .then(data => {
+            const {name, id} = data;
+            const {
+                sprites: {
+                    other: {
+                        home: {front_default: spriteLink}
+                    }
+                }, 
+            } = data;
+            let featuredInfo = {name, id, spriteLink};
+            return featuredInfo;
+        }).then(data => {
+            console.log(data);
+            // I want to use this box to put the 3 featured pokemon in!
+            featBoxNodes.forEach(box => {
+                // console.log(box.childNodes);
+                insertFeaturedPokemon(data);
+            });
+        }
+        )
     });
+}
+
+function insertFeaturedPokemon(obj) {
+    let pokemonID = document.getElementsByClassName('featID');
+    let pokemonName = document.getElementsByClassName('featName');
+    let spriteSRC = document.getElementsByClassName('featImage').src;
+    pokemonID.innerHTML = obj.id;
+    pokemonName.innerHTML = obj.name;
+    spriteSRC = obj.spriteLink;
+
+
 }
 
 getFeaturedIDs(); // THIS FUNCTION SHOULD ONLY RUN ONCE A DAY, don't want new pokemon with every refresh
